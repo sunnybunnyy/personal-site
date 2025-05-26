f0351477444d48898faf54abde1cdc40cb2035c9
Radha Kotra <kradha6113@gmail.com>
2025-05-26 10:37:49 -0400

    Add NavBar component to page

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import NavBar from '../components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
